3e469bef577a6799199442ba8b1151fd82c4e66d
Tsusu <134057585+tsusu0409@users.noreply.github.com>
2025-01-07 01:50:43 +0900

    Create a.cpp

---
+++ ABC/387/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+
+int main() {
+  int A, B;
+  int ans;
+  cin >> A >> B;
+  ans = (A + B) * (A + B);
+  cout << ans << endl;
+}
