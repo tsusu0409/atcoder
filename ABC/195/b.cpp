d4eeba4a846fc2f20d77bfd1164f577d2a4eb6bb
tsusu0409 <tsubasa.kawagishi@gmail.com>
2025-02-08 01:58:25 +0900

    WA'd abc195-b

---
+++ ABC/195/b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+
+int main() {
+    int A, B, W;
+    cin >> A >> B >> W;
+    W *= 1000;
+    
+}
