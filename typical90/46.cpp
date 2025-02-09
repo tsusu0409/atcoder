d06e65471accd1e988b25c97610ab46b0cbc0077
tsusu0409 <tsubasa.kawagishi@gmail.com>
2025-02-10 00:42:14 +0900

    during typical90-46

---
+++ typical90/46.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    vector<int> B(N);
+    vector<int> C(N);
+    rep(i, N){
+        cin >> A[i];
+    }
+    rep(i, N){
+        cin >> B[i];
+    }
+    rep(i, N){
+        cin >> C[i];
+    }
+    
+}
