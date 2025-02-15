4f0d61b8c2b9dec7779611f504cc310922c30495
tsusu0409 <tsubasa.kawagishi@gmail.com>
2025-02-15 22:07:46 +0900

    joined abc393

--- ABC/393/d.cpp
+++ ABC/393/d.cpp
@@ -9,5 +9,23 @@ using P = pair<int,int>;
 int main() {
     int N; cin >> N;
     string S; cin >> S;
-    
+    int sum = 0;
+    rep(i, N){
+        sum += S[i] - '0';
+    }
+
+    int maSum = 0;
+    rep(i, N-sum){
+        int count = 0;
+        rep(j, sum){
+            if(S[j] == '1'){
+                count ++;
+            }
+        }
+        maSum = max(count, maSum);
+    }
+
+    cout << sum - maSum << endl;
+
+    // 問題文誤読してた、そりゃ無理や
 }
