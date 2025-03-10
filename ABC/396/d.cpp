7da6b88d2ff2313534e4c99c9e89571e56c8fc59
tsusu0409 <tsubasa.kawagishi@gmail.com>
2025-03-11 02:34:57 +0900

    learned dfs with abc396-d

--- ABC/396/d.cpp
+++ ABC/396/d.cpp
@@ -6,66 +6,39 @@ using namespace atcoder;
 using ll = long long;
 using P = pair<int,int>;
 
-struct edge {
-    int to;
-    ll weight;
-};
-
-using Graph = vector<vector<edge>>;
-
 int main() {
     int N, M; cin >> N >> M;
-    Graph G(N);
+    vector<vector<pair<int, ll>>> G(N);
     rep(i, M){
         int u, v; ll w;
         cin >> u >> v >> w;
-        G[u].push_back(edge(v, w));
-        G[v].push_back(edge(u, w));
+        u--;
+        v--;
+        G[u].push_back({v, w});
+        G[v].push_back({u, w});
     }
 
-    vector<string> dist[G.size()];
-    fill(dist, dist + G.size(), -1);
-    dist[s] = 0;
-
-    priority_queue<P, vector<P>, greater<P>> que;
-    que.push({0, 0});
-
-    while(!que.emplty()){
-        P c = que.top();
-        int d = c.first;
-        int idx = c.second;
-        que.pop();
-        ll si = 0;
-        int check = 1;
-        rep(i, dist[idx].size()){
-            si += check * dist[idx][j];
-            check *= 2;
+    ll ans = 2e18;
+    auto dfs = [&](auto dfs, int s, vector<bool> visited, ll score) -> void{
+        visited[s] = 1;
+        if(s == N-1){
+            ans = min(ans, score);
+            return;
         }
-        if(d > si){
-            continue;
-        }
-        for(auto i: G[idx]){
-            int to = G[idx][i].to;
-            int weight = grapsh[idx][i].weight;
-            bitset<60> bs(weight);
-            vector<int> new(60);
-            rep(j, 60){
-                if(bs[i] == 0 && dist[to][i] == '0'){
-                    new[i] = 0;
-                }
-                else if(bs[i] == 1 && dist[to][i] == '1'){
-                    new[i] = 0;
+        else{
+            for(auto [to, weight]: G[s]){
+                if(visited[to]){
+                    continue;
                 }
                 else{
-                    new[i] = 1;
+                    dfs(dfs, to, visited, score^weight);
                 }
             }
-            rep(j, 60){
-                dist[to][i] == new[i];
-            }
-            que.push({dist[to], to});
         }
-    }
+    };
 
-    cout << dist[N-1] << endl;
+    dfs(dfs, 0, vector<bool>(N, 0), 0);
+
+    cout << ans << endl;
+
 }
