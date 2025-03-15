333779808f2d87454091f1eadc9b791fde18d822
tsusu0409 <tsubasa.kawagishi@gmail.com>
2025-03-16 02:38:54 +0900

    retired typical90-26

---
+++ typical90/26.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using P = pair<int,int>;
+
+int main() {
+    int N; cin >> N;
+    
+}
