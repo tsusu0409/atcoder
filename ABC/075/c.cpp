69bfe23b80c3bdd0706b8e2ef2d66532beb6d08b
tsusu0409 <tsubasa.kawagishi@gmail.com>
2025-03-16 02:25:23 +0900

    WA'd abc075-c

---
+++ ABC/075/c.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using P = pair<int,int>;
+
+int main() {
+    int N, M; cin >> N >> M;
+    vector<P> input(M);
+    rep(i, M){
+        int a,b; cin >> a >> b;
+        input[i] = {a, b};
+    }
+
+    int ans = 0;
+    rep(i, M){
+        vector<vector<int>> g(N);
+        rep(j, M){
+            if(i==j){
+                continue;
+            }
+            int ap = input[j].first;
+            int bp = input[j].second;
+            ap--; bp--;
+            g[ap].push_back(bp);
+            g[bp].push_back(ap);
+        }
+
+
+        auto dfs = [&](auto dfs, vector<bool> visited, int s) -> void {
+            visited[s] = 1;
+            int check = 0;
+            for(auto i: g[s]){
+                if(i == 0){
+                    check = 1;
+                }
+            }
+            if(!check){
+                return;
+            }
+        
+            for(auto i: g[s]){
+                if(visited[i] == 1){
+                    continue;
+                }
+                else{
+                    dfs(dfs, visited, i);
+                }
+            }
+        };
+
+        vector<bool> visited(N, 0);
+        dfs(dfs, visited, 0);
+
+        int count = 0;
+        for(auto j: visited){
+            if(j){
+                count ++;
+            }
+        }
+        if(count != N){
+            ans ++;
+        }
+    }
+
+    cout << ans << endl;
+}
