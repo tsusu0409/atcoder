add7ae84c974d1d5568bbe9965756e7dec1d827c
tsusu0409 <tsubasa.kawagishi@gmail.com>
2025-03-16 18:06:41 +0900

    WA'd abc084-d

---
+++ ABC/084/d.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+
+bool check_prime(int N, vector<int> prime) {
+    for(auto i: prime)    
+}
+
+int main() {
+    int Q; cin >> Q;
+    int N = 100000;
+    vector<bool> check(N);
+    rep(i, N){
+        
+    }
+
+    // 素数判定どうすればいいかわからないそぽ～～～
+}
