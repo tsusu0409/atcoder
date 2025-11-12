a4689ad4bb97933bdfd10265ffdc471c7b5c8a45
tsusu0409 <tsubasa.kawagishi@gmail.com>
2025-11-13 00:56:12 +0900

    AC'd abc213-a

---
+++ ABC/200/213/a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+
+int main() {
+    int A, B; cin >> A >> B;
+    cout << (A^B) << endl;
+}
